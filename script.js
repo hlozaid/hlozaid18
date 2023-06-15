ef54b328b1a8e0a97213f94922af3703182099ee
hlozaid <95207433+hlozaid@users.noreply.github.com>
2023-06-15 21:20:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
-// toggle icon navbar
- 
-// scroll sections
-Let sections = document.querySelectorAll('section')
-Let nqvLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = > () => {
-    sections.forEach(sec => {
-        Let top = window.scrollY;
-        Let offset = sec.offsetTop - 100;
-        Let height = sec.offsetHeight;
-        Let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            //active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href#=' + id +']').classList.add('active');
-            })
-      
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
-}
- 
-    // sticky header
-    let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
-    // remove toggle icon and navbar when click navbar links (scroll)
- 
-    // animation footer on scroll
-}
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Software Developer",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["software Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
